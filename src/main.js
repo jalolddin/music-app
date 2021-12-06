a9476575e471a2e0ff456ccfa1e10a0c5ce5c1bc
Jaloliddin <ibrokhimovjaloliddin@gmail.com>
2021-12-06 22:44:51 +0500

    music

--- src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import './assets/style.css'
-const fs = require('fs');
-// const __dirname = require()
-// const app = express();
-// app.use(express.static(require(__dirname)))
-createApp(App).mount('#app')
